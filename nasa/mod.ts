2cf1542dc7da045f1c9bbd5cafe4763c29bac371
r1oga <r1oga@tuta.io>
2020-06-14 10:48:33 +0200

    Set X-Response-Time header to measure time to set the request body

--- nasa/mod.ts
+++ nasa/mod.ts
@@ -1,9 +1,25 @@
 import { Application } from 'https://deno.land/x/oak@v5.2.0/mod.ts'
+import * as log from 'https://deno.land/std/log/mod.ts'
 
 const app = new Application()
 const PORT = 8000
 
-app.use(({ response }) => {
+app.use(async ({ request, response }, next) => {
+  await next()
+  const time = response.headers.get('X-Response-Time')
+  log.info(`${request.method} ${request.url}: ${time}`)
+})
+
+app.use(async ({ response }, next) => {
+  const start = Date.now()
+  await next()
+  // delta is calculated after the next() promise resolves.
+  // ie after the response body is set and returned
+  const delta = Date.now() - start
+  response.headers.set('X-Response-Time', `${delta}ms`)
+})
+
+app.use(async ({ response }, next) => {
   response.body = `
 88888b.  8888b. .d8888b  8888b.
 888 "88b    "88b88K         "88b
@@ -13,6 +29,7 @@ app.use(({ response }) => {
 
       Mission Control API
 `
+  await next()
 })
 
 // only if executed as a program instead of imported as a module
